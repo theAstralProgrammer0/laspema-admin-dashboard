6fd2f8af75effcc343cfa61dcc39eddd7ae9fe67
Temitayo Daisi-Oso <tdaisioso@gmail.com>
2023-05-08 03:09:30 +0100

    Frontend working perfectly, dataUser inserted into database, Server running perfectly

--- server/index.js
+++ server/index.js
@@ -10,6 +10,10 @@ import generalRoutes from './routes/general.js';
 import managementRoutes from './routes/management.js';
 import salesRoutes from './routes/sales.js';
 
+/** data imports */
+import User from "./models/User.js";
+import { dataUser } from "./data/index.js";
+
 /** CONFIGURATION */
 dotenv.config();
 const app = express();
@@ -36,5 +40,8 @@ mongoose
     })
     .then(() => {
         app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+
+        /** 1 DATA INSERTION (ONCE!) EACH */
+        // User.insertMany(dataUser);
     })
     .catch((error) => console.log(`${error} did not connect`));
