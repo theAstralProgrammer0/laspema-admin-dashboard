61524a542ba851d4892fdc8a33ac9f152bd4912d
theAstralProgrammer0 <tdaisioso@gmail.com>
2025-02-02 20:41:56 +0100

    Committed all unsaved changes

--- server/controllers/client.js
+++ server/controllers/client.js
@@ -1,77 +1,100 @@
-import Product from "../models/Product.js"
-import ProductStat from "../models/ProductStat.js"
+import Product from "../models/Product.js";
+import ProductStat from "../models/ProductStat.js";
+import User from "../models/User.js";
 import Transaction from "../models/Transaction.js";
-import User from "../models/User.js"
+import getCountryIso3 from "country-iso-2-to-3";
 
 export const getProducts = async (req, res) => {
-    try{
-        const products = await Product.find();
+  try {
+    const products = await Product.find();
 
-        const productsWithStats = await Promise.all(
-            products.map(async (product) => {
-                const stat = await ProductStat.find({
-                    productId: product._id
-                })
-                return {
-                    ...product._doc,
-                    stat,
-                }
-            })
-        );
+    const productsWithStats = await Promise.all(
+      products.map(async (product) => {
+        const stat = await ProductStat.find({
+          productId: product._id,
+        });
+        return {
+          ...product._doc,
+          stat,
+        };
+      })
+    );
 
-        res.status(200).json(productsWithStats);
-
-    } catch (error){
-        res.status(404).json({ msg: "Error exists here 2" });
-    }
-}
-
+    res.status(200).json(productsWithStats);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+};
 
 export const getCustomers = async (req, res) => {
-    try {
-        const customers = await User.find({ role: "user" }).select("-password");
-        res.status(200).json(customers)
-    } catch (error) {
-        res.status(404).json({ msg: "error lives here 3"})
-        
-    }
-}
-
+  try {
+    const customers = await User.find({ role: "user" }).select("-password");
+    res.status(200).json(customers);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+};
 
 export const getTransactions = async (req, res) => {
-    try {
-        // sort should look like this: { "field": "userId", "sort": "desc"}
-        const { page = 1, pageSize = 20, sort = null, search = "" } = req.query;
+  try {
+    // sort should look like this: { "field": "userId", "sort": "desc"}
+    const { page = 1, pageSize = 20, sort = null, search = "" } = req.query;
 
-        const generateSort = () => {
-            const sortParsed = JSON.parse(sort);
-            const sortFormatted = {
-                [sortParsed.field]: sortParsed.sort = "asc" ? 1 : -1
-            };
+    // formatted sort should look like { userId: -1 }
+    const generateSort = () => {
+      const sortParsed = JSON.parse(sort);
+      const sortFormatted = {
+        [sortParsed.field]: (sortParsed.sort = "asc" ? 1 : -1),
+      };
 
-            return sortFormatted;
-        }
-        const sortFormatted = Boolean(sort) ? generateSort() : {};
+      return sortFormatted;
+    };
+    const sortFormatted = Boolean(sort) ? generateSort() : {};
 
-        const transactions = await Transaction.find({
-            $or: [
-                { cost: { $regex: new RegExp(search, "i") } },
-                { userId: { $regex: new RegExp(search, "i") } }
-            ]
-        })
-        .sort(sortFormatted)
-        .skip(page * pageSize)
-        .limit(pageSize);
+    const transactions = await Transaction.find({
+      $or: [
+        { cost: { $regex: new RegExp(search, "i") } },
+        { userId: { $regex: new RegExp(search, "i") } },
+      ],
+    })
+      .sort(sortFormatted)
+      .skip(page * pageSize)
+      .limit(pageSize);
 
-        const total = await Transaction.countDocuments({
-            name: { $regex: search, $options: "i"}
-        })
+    const total = await Transaction.countDocuments({
+      name: { $regex: search, $options: "i" },
+    });
 
-        res.status(200).json({
-            transactions,
-            total
-        });
-    } catch (error) {
-        res.status(404).json({ error: "error is inside this place"})
-    }
-}
+    res.status(200).json({
+      transactions,
+      total,
+    });
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+};
+
+export const getGeography = async (req, res) => {
+  try {
+    const users = await User.find();
+
+    const mappedLocations = users.reduce((acc, { country }) => {
+      const countryISO3 = getCountryIso3(country);
+      if (!acc[countryISO3]) {
+        acc[countryISO3] = 0;
+      }
+      acc[countryISO3]++;
+      return acc;
+    }, {});
+
+    const formattedLocations = Object.entries(mappedLocations).map(
+      ([country, count]) => {
+        return { id: country, value: count };
+      }
+    );
+
+    res.status(200).json(formattedLocations);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+};
